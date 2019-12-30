998f06db26ebcd2d4eed840256d8158dbfedb8f5
Kimo <jm865@kent.ac.uk>
2019-12-30 01:47:25 +0000

    Updates

---
+++ precache-manifest.7406e7ab7fa5cf49d6a1bf216297446e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d329fd74ba1754d1a0a2d9e855cad747",
+    "url": "/complete-react-dev-course/index.html"
+  },
+  {
+    "revision": "8846527fa0a651e3e1a4",
+    "url": "/complete-react-dev-course/static/css/main.8bf00fca.chunk.css"
+  },
+  {
+    "revision": "a0aad4e16871875ff344",
+    "url": "/complete-react-dev-course/static/js/2.6277ae1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/complete-react-dev-course/static/js/2.6277ae1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8846527fa0a651e3e1a4",
+    "url": "/complete-react-dev-course/static/js/main.dae11f30.chunk.js"
+  },
+  {
+    "revision": "c784fafaf27a2c01548a",
+    "url": "/complete-react-dev-course/static/js/runtime-main.f463b7a5.js"
+  }
+]);
