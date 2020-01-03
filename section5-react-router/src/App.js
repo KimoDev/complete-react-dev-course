f7a769987056d554fdaefef1e3978f84e0763a67
Kimo <jm865@kent.ac.uk>
2020-01-03 00:46:57 +0000

    Added withRouter HOC

--- section5-react-router/src/App.js
+++ section5-react-router/src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import { Switch, Link, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HomePage2 = props => {
-  console.log(props);
-  return (
-    <div>
-      <Link to="/hats">Hats</Link>
-      <button onClick={() => props.history.push('/hats')}>hi</button>
-      <h1>Home page 2</h1>
-    </div>
-  )
-}
 
 const HatsPage = (props) => {
   console.log(props)
@@ -33,14 +22,12 @@ const HatsDetails = () => {
   )
 }
 
-
-
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage2}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
         <Route path='/hats/:id' component={HatsDetails}/>
       </Switch>
     </div>
