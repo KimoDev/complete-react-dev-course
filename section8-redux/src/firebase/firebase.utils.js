818a291004d7152b38e80edd149e84a3978dc60b
Kimo <jm865@kent.ac.uk>
2020-08-15 00:02:58 +0100

    Added section8 folder

---
+++ section8-redux/src/firebase/firebase.utils.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName, 
+        email, 
+        createdAt,
+        ...additonalData
+      })
+    } catch (error) {
+      console.log('error creating user ', error)
+    }
+  }
+  
+  return userRef;
+}
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'})
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
